2feddc4a49f6024921c1ff5660dc4835c1d2add6
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-09 19:16:43 -0300

    escribi como un infeliz y no funciona no funciona. revisar despues de clases

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-import "./styles.css";
+rotulo1.innerHTML = "ingrese el primer numero";
+rotulo2.innerHTML = "ingrese el segundo numero";
+rotulo3.innerHTML = "ingrese el tercer numero";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+btn1.addEventListener("click", () => {
+  let dato1: number = Number(dato1.value);
+  let dato2: number = Number(dato2.value);
+  let dato3: number = Number(dato3.value);
+  let dato1 = document.getElementById(dato1);
+  let dato2 = document.getElementById(dato2);
+  let dato3 = document.getElementById(dato3);
+
+  if (dato1 > dato2 && dato1 > dato3) {
+    console.log("el numero " + dato1 + "es el mayor");
+  } else if (dato2 > dato1 && dato2 > dato3) {
+    console.log("el numero " + dato2 + "es el mayor");
+  } else if (dato3 > dato1 && dato3 > dato2) {
+    console.log("el numero " + dato3 + "es el mayor");
+  }
+});
