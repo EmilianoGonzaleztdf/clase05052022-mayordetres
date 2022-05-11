98b0c7ef60bc7e4fd002f18b79a3190a123a389f
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-10 23:38:12 -0300

    esta mal pero no tan mal ya funciona, pero si pongo un valor mayor a 10 CRASH

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,20 @@ rotulo1.innerHTML = "ingrese el primer numero";
 rotulo2.innerHTML = "ingrese el segundo numero";
 rotulo3.innerHTML = "ingrese el tercer numero";
 
-btn1.addEventListener("click", () => {
-  let dato1: number = Number(dato1.value);
-  let dato2: number = Number(dato2.value);
-  let dato3: number = Number(dato3.value);
-  let dato1 = document.getElementById(dato1);
-  let dato2 = document.getElementById(dato2);
-  let dato3 = document.getElementById(dato3);
+let numero1 = document.getElementById("dato1");
+let numero2 = document.getElementById("dato2");
+let numero3 = document.getElementById("dato3");
 
-  if (dato1 > dato2 && dato1 > dato3) {
-    console.log("el numero " + dato1 + "es el mayor");
-  } else if (dato2 > dato1 && dato2 > dato3) {
-    console.log("el numero " + dato2 + "es el mayor");
-  } else if (dato3 > dato1 && dato3 > dato2) {
-    console.log("el numero " + dato3 + "es el mayor");
+btn1.addEventListener("click", () => {
+  let numero1: number = dato1.value;
+  let numero2: number = dato2.value;
+  let numero3: number = dato3.value;
+
+  if (numero1 > numero2 && numero1 > numero3) {
+    console.log("El numero " + numero1 + " es el mayor");
+  } else if (numero2 > numero1 && numero2 > numero3) {
+    console.log("El numero " + numero2 + " es el mayor");
+  } else if (numero3 > numero1 && numero3 > numero1) {
+    console.log("El numero " + numero3 + " es el mayor");
   }
 });
